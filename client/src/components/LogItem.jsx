d89ebbff176a78295c7eff70dd4321f1a99e7fd7
leesuehee <sueheelee@outlook.com>
2018-05-14 16:24:08 -0700

    Includes photos of items when searched

--- client/src/components/LogItem.jsx
+++ client/src/components/LogItem.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 const LogItem = (props) => (
 
   <div className='item-info'>
+    {console.log(props)}
     <ul className='name'>{props.info.foods[0].food_name}</ul>
-    <ul className='cal'>{props.info.foods[0].nf_calories}</ul>
-    
+    <ul className='cal'>calories {props.info.foods[0].nf_calories}</ul>
+  <div className='item-img'>
+    <img src={props.info.foods[0].photo.thumb}/>
+  </div>
   </div>
 )
 
 export default LogItem;
 
-// <div className ='name'> {props.item} </div>
-// <div className ='calories'> {props.calories} </div>
 
-// <div className ='img'>
-//   <img src={props.img}/>
-// </div>
+
+
+
