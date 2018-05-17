c3af5f1216c14828c6a35548dc59ac359d36f2d2
leesuehee <sueheelee@outlook.com>
2018-05-17 12:32:01 -0700

    Adds skeleton of CalorieCart component

---
+++ client/src/components/CalorieCart.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// db joined here?
+
+let CalorieCart = (props) => {
+  // this component is a cart for adding food items only 
+  // prereq
+    // button on log to add the current foot item to the cart
+  
+  // reqs 
+
+
+}
+
+export default CalorieCart;
